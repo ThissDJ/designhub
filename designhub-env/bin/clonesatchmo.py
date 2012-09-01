e7ca1639e31c37235b1cdecf1d2abdf19b5523cd
ThissDJ <admin@yingyuexiangce.com>
2012-09-01 00:10:43 -0500

    Satchmo version

---
+++ designhub-env/bin/clonesatchmo.py
@@ -0,0 +1,5 @@
+#!/home/designhub/webapps/designhub/designhub-env/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'Satchmo==0.9.3','clonesatchmo.py'
+__requires__ = 'Satchmo==0.9.3'
+import pkg_resources
+pkg_resources.run_script('Satchmo==0.9.3', 'clonesatchmo.py')
--- testshop/api/resources.py
+++
@@ -1,8 +0,0 @@
-from djangorestframework.resources import ModelResource
-from testshop.shop.models import Comment
-
-class CommentResource(ModelResource):
-    model = Comment
-    allowed_methods = ('GET','POST',)
-    ordering = ('-created',)
-    fields = ('id',('product',('id',)),('user',('username','id',)),'content','url','created',)
