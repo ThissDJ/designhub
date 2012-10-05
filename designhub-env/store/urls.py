cc8fb622da1a31308d86337f3c5390219b5e5dde
ThissDJ <admin@yinyuexiangce.com>
2012-10-05 18:59:29 +0800

    start using local_setting.py

--- designhub-env/store/urls.py
+++ designhub-env/store/urls.py
@@ -8,22 +8,44 @@ from localsite.facebook import facebook_view
 #designers
 from localsite.views import DesignerListView, DesignerDetailView, DesignerAjaxListView
 
-urlpatterns += patterns('',
-    url(r'^comments/', include('django.contrib.comments.urls')),
-    url(r"^talents/$", DesignerListView.as_view(),{'SSL': True}, 
-            name='designer-list'),
-    url(r"^talents/ajax/$", DesignerAjaxListView.as_view(), 
-            name='designer-list-ajax'),
-    url(r'^fbtab/$','localsite.views.fabtabFeaturedToday'),
-    
-    url(r"^talents/(?P<slug>[\-\/\w]+)/$", DesignerDetailView.as_view(),name="designer-detail"),     
-    url(r"^sale/$", 'localsite.views.saleindex', name='sale-index'),          
-    url(r'^social/$', home, name='home'),
-    url(r'^social/done/$', done, name='done'),
-    url(r'^social/error/$', error, name='error'),
-    url(r'^social/logout/$', logout, name='logout'),
-    url(r'^social/form/$', form, name='form'),
-    url(r'^social/form2/$', form2, name='form2'),
-    url(r'^social/fb/', facebook_view, name='fb_app'),
-    url(r'', include('social_auth.urls')),
-)
+from local_settings import LOCAL_DEV
+if LOCAL_DEV:
+    urlpatterns += patterns('',
+        url(r'^comments/', include('django.contrib.comments.urls')),
+        url(r"^talents/$", DesignerListView.as_view(), 
+                name='designer-list'),
+        url(r"^talents/ajax/$", DesignerAjaxListView.as_view(),
+                name='designer-list-ajax'),
+        url(r'^fbtab/$','localsite.views.fabtabFeaturedToday'),
+        
+        url(r"^talents/(?P<slug>[\-\/\w]+)/$", DesignerDetailView.as_view(), name="designer-detail"),     
+        url(r"^sale/$", 'localsite.views.saleindex', name='sale-index'),          
+        url(r'^social/$', home, name='home'),
+        url(r'^social/done/$', done, name='done'),
+        url(r'^social/error/$', error, name='error'),
+        url(r'^social/logout/$', logout, name='logout'),
+        url(r'^social/form/$', form, name='form'),
+        url(r'^social/form2/$', form2, name='form2'),
+        url(r'^social/fb/', facebook_view, name='fb_app'),
+        url(r'', include('social_auth.urls')),
+    )
+else:
+    urlpatterns += patterns('',
+        url(r'^comments/', include('django.contrib.comments.urls')),
+        url(r"^talents/$", DesignerListView.as_view(),{'SSL': True}, 
+                name='designer-list'),
+        url(r"^talents/ajax/$", DesignerAjaxListView.as_view(), {'SSL': True},
+                name='designer-list-ajax'),
+        url(r'^fbtab/$','localsite.views.fabtabFeaturedToday',{'SSL': True}),
+        
+        url(r"^talents/(?P<slug>[\-\/\w]+)/$", DesignerDetailView.as_view(),{'SSL': True}, name="designer-detail"),     
+        url(r"^sale/$", 'localsite.views.saleindex',{'SSL': True}, name='sale-index'),          
+        url(r'^social/$', home,{'SSL': True}, name='home'),
+        url(r'^social/done/$', done,{'SSL': True}, name='done'),
+        url(r'^social/error/$', error,{'SSL': True}, name='error'),
+        url(r'^social/logout/$', logout,{'SSL': True}, name='logout'),
+        url(r'^social/form/$', form,{'SSL': True}, name='form'),
+        url(r'^social/form2/$', form2,{'SSL': True}, name='form2'),
+        url(r'^social/fb/', facebook_view,{'SSL': True}, name='fb_app'),
+        url(r'', include('social_auth.urls')),
+    )
